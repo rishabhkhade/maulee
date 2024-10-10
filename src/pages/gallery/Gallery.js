9449739314a814375f90d4493c58c22a6d2b76ab
ketanrd26 <ketandudka566123@gmail.com>
2024-10-10 20:49:18 +0530

    push

--- src/pages/gallery/Gallery.js
+++ src/pages/gallery/Gallery.js
@@ -1,46 +1,109 @@
-import React from 'react'
-import "./gallery.scss"
-import Pagetop from '../../comp/pagetop/Pagetop'
+import React, { useState } from "react";
+import "./gallery.scss";
+import Pagetop from "../../comp/pagetop/Pagetop";
+
+// image
+import image from "../../assets/IMG_9164.jpg";
 const Gallery = () => {
+  const images = [
+    {
+      image: image,
+    },
+    {
+      image: image,
+    },
+    {
+      image: image,
+    },
+    {
+      image: image,
+    },
+    {
+      image: image,
+    },
+    {
+      image: image,
+    },
+    {
+      image: image,
+    },
+    {
+      image: image,
+    },
+    {
+      image: image,
+    },
+    {
+      image: image,
+    },
+    {
+      image: image,
+    },
+    {
+      image: image,
+    },
+    {
+      image: image,
+    },
+    {
+      image: image,
+    },
+  ];
+
+  const [gallerypages, setgalleryPage] = useState({
+    photos: true,
+    videos: false,
+    ytlinks: false,
+  });
+
+  const closeAll = () => {
+    setgalleryPage({
+      photos: true,
+      videos: false,
+      ytlinks: false,
+    });
+  };
   return (
     <>
-      <Pagetop  pageHeader="Maulees's Work" />
+      <Pagetop pageHeader="Maulees's Work" />
 
       <div className="gallery-parent parent">
         <div className="gallery-cont cont">
-            <div className="btn-list">
-                <div className="btn-tag">
-                    Photos
-                </div>
-                <div className="btn-tag">
-                    Videos
-                </div>
-                <div className="btn-tag">
-                    YT Links
-                </div>
+          <div className="btn-list">
+            <div
+              className={gallerypages.photos ? "btn-tag active" : "btn-tag"}
+              onClick={() =>
+                setgalleryPage({
+                  ...gallerypages,
+                  photos: true,
+                },
+                closeAll())
+                
+              }
+            >
+              Photos
             </div>
+            <div className="btn-tag">Videos</div>
+            <div className="btn-tag">YT Links</div>
+          </div>
 
-            <div className="gallery-section">
-              <div className="div1  class"></div>
-              <div className="div2  class"></div>
-              <div className="div3  class"></div>
-              <div className="div4  class"></div>
-              <div className="div5  class"></div>
-              <div className="div6  class"></div>
-              <div className="div7  class"></div>
-              <div className="div8  class"></div>
-              <div className="div9  class"></div>
-              <div className="div10  class"></div>
-              <div className="div11  class"></div>
-              <div className="div12  class"></div>
-              <div className="div13  class"></div>
-              <div className="div14  class"></div>
-               
-            </div>
+          <div className="gallery-section">
+        {gallerypages.photos &&
+          <>
+          {images.map((item, index) => (
+                <div
+                  className=" class bg-img-cover"
+                  style={{ backgroundImage: `url(${item.image})` }}
+                  key={index}
+                ></div>
+              ))}
+          </>
+        }
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
