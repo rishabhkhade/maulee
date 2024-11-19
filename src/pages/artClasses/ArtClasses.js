9086c5c74ed75ebdd46b8fda1e7d5c6d2d02c2c2
ketanrd26 <ketandudka566123@gmail.com>
2024-11-19 18:27:20 +0530

    push

--- src/pages/artClasses/ArtClasses.js
+++ src/pages/artClasses/ArtClasses.js
@@ -2,29 +2,69 @@ import React from "react";
 import Pagetop from "../../comp/pagetop/Pagetop";
 import "./artclasses.scss";
 
-
 // icons
 
-import mission_icon from "../../assets/mission_icon.png"
+import mission_icon from "../../assets/mission_icon.png";
+import values_icon from "../../assets/values.png";
+import Journey_icon from "../../assets/journey.png";
+import SectionHeading from "../../comp/sectionHeading/SectionHeading";
 const ArtClasses = () => {
   const vismisContent = [
     {
-      title: "Our Vision",
+      title: "Our MIssion",
       description:
         "Moulees Tattoo Studio empowers aspiring tattoo artists by blending craftsmanship, innovation, and creativity, fostering appreciation and artistic growth.",
-        icon:mission_icon
+      icon: mission_icon,
     },
     {
-      title: "Our Vision",
+      title: "Our Values",
       description:
-        "Moulees Tattoo Studio empowers aspiring tattoo artists by blending craftsmanship, innovation, and creativity, fostering appreciation and artistic growth.",
-        icon:mission_icon
+        "We value artistic excellence, creativity, individuality, inclusivity, ethical practices, and nurturing aspiring tattoo artists with professionalism and artistry.",
+      icon: values_icon,
     },
     {
-      title: "Our Vision",
+      title: "Our Journey",
       description:
-        "Moulees Tattoo Studio empowers aspiring tattoo artists by blending craftsmanship, innovation, and creativity, fostering appreciation and artistic growth.",
-        icon:mission_icon
+        "From art lovers to seasoned professionals, we became Pune’s top tattoo studio, offering expert 6-month tattooing courses.",
+      icon: Journey_icon,
+    },
+  ];
+
+  const curriculam = [
+    {
+      id: 1,
+      title: "Artistic Vision",
+      heading:
+        "We will train individuals' minds as artists and successful individuals.",
+      points: [
+        "Tattoo history and culture",
+        "Detailed introduction to the tattoo industry across India and internationally",
+        "How to push our minds to improve with each endeavor, both artistically and commercially.",
+        "Our syllabus is designed to train you in skills as well as subconsciously, technically, and spiritually.",
+      ],
+    },
+    {
+      id: 2,
+      title: "Artistic Skill Set",
+      heading: "Tattooing is application of five art on a special medium.",
+      points: [
+        "We have created a syllabus from real life experience so it seems the purpose to teach anyone even from non- artistic background.",
+        "Only one criterion is love towards art and passion in heart to work for it.",
+        "Creativity and innovation",
+        "Understanding Skin structure",
+      ],
+    },
+    {
+      id: 3,
+      title: "Designing Skills",
+      heading:
+        " Master tattoo design, line work, shading, composition, balance, and style.",
+      points: [
+        "Artistic Fundamentals",
+        "Customization and Personalization",
+        "Anatomy and Placement",
+        "Ethical Considerations",
+      ],
     },
   ];
   return (
@@ -45,12 +85,42 @@ const ArtClasses = () => {
 
       <div class="vismis-parent parent">
         <div class="vismis-cont cont">
-          {vismisContent.map((item , index)=>(
-            <div class="vismis-data">
-            <div class="left-icon"></div>
-            <div class="right-content"></div>
-          </div>
+          {vismisContent.map((item, index) => (
+            <div class="vismis-data" key={index}>
+              <div class="left-icon bg-img-contain">
+                <img src={item.icon} alt="" />
+              </div>
+              <div class="right-content">
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+              </div>
+            </div>
           ))}
+        </div>
+      </div>
+
+      <div class="curriculam-parent parent">
+        <div class="curriculam-cont cont">
+          <SectionHeading
+            line_text="Curriculum"
+            fill_text="Curriculum"
+       
+          />
+          <div class="top">
+            {curriculam.map((item, index) => (
+              <div class="card-data">
+                <div class="index">{item.id}</div>
+                <h5>{item.heading}</h5>
+                <ul>
+                  {item.points.map((item) => (
+                    <li>{item}</li>
+                  ))}
+                </ul>
+
+                <ul></ul>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
